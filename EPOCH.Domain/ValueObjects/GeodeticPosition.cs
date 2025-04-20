eff45649fbcc49366e7c7809014f400b45c04f20
inc <goats4life@yandex.ru>
2025-04-20 23:40:55 +0300

    Reapply "feat: add GeodeticPostion ValueObject"
    This reverts commit 21194359ea318aa0f8951b36e72708881543ed90.

---
+++ EPOCH.Domain/ValueObjects/GeodeticPosition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPOCH.Domain.ValueObjects
+{
+    public sealed record GeodeticPosition {
+
+        public double Latitude { get; }
+        public double Longitude { get; }
+        public double Altitude { get; }
+
+        public GeodeticPosition(double latitude, double longitude, double altitude)
+        {
+            if (latitude is < -90 or > 90)
+                throw new ArgumentException("Latitude must be between -90 and 90 degrees.");
+
+            if (longitude is < -180 or > 180)
+                throw new ArgumentException("Longitude must be between -180 and 180 degrees.");
+
+            if (altitude < 0)
+                throw new ArgumentException("Altitude must be non-negative.");
+
+            Latitude = latitude;
+            Longitude = longitude;
+            Altitude = altitude;
+        }
+    }
+}

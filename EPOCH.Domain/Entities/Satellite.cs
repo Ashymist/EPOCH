d2b87ac2791996351a7fdcdbaf9e1f7de212dc1f
inc <goats4life@yandex.ru>
2025-04-20 16:34:58 +0300

    chore: create initial setup

--- EPOCH.Domain/Entities/Satellite.cs
+++ EPOCH.Domain/Entities/Satellite.cs
@@ -1,10 +1,6 @@
 ﻿using EPOCH.Domain.Enums;
 using EPOCH.Domain.ValueObjects;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EPOCH.Domain.Entities
 {

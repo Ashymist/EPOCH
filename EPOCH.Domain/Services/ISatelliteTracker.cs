0b77ed7a38048c3beae93e5bfaae71f2ce76c88e
inc <goats4life@yandex.ru>
2025-04-22 01:12:15 +0300

    feat: add interface for tracking satellites

---
+++ EPOCH.Domain/Services/ISatelliteTracker.cs
@@ -0,0 +1,15 @@
+﻿using EPOCH.Domain.Entities;
+using EPOCH.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPOCH.Domain.Services
+{
+    public interface ISatelliteTracker
+    {
+        GeodeticPosition GetGeodeticPosition(Satellite satellite, DateTime targetTime);
+    }
+}

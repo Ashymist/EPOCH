253c7380177672d5800bcf1b37a7bcf465b044b5
inc <goats4life@yandex.ru>
2025-04-22 01:16:55 +0300

    feat: add interface for classifying satellites

---
+++ EPOCH.Domain/Services/ISatelliteClassifier.cs
@@ -0,0 +1,15 @@
+﻿using EPOCH.Domain.Entities;
+using EPOCH.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPOCH.Domain.Services
+{
+    public interface ISatelliteClassifier
+    {
+        SatelliteGroup GetSatelliteGroup(Satellite satellite);
+    }
+}

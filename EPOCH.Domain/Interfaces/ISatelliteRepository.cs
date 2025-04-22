12bedb7819d1abae9c28ae1ca10b74a25cb07c4f
inc <goats4life@yandex.ru>
2025-04-22 12:57:32 +0300

    feat: add interface for repository pattern

---
+++ EPOCH.Domain/Interfaces/ISatelliteRepository.cs
@@ -0,0 +1,18 @@
+﻿using EPOCH.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPOCH.Domain.Interfaces
+{
+    public interface ISatelliteRepository
+    {
+        Task<Satellite?> GetByNoradIdAsync(int noradId);
+        Task<IEnumerable<Satellite>> GetAllAsync();
+        Task RefreshAllAsync(IEnumerable<Satellite> satellites);
+        Task DeleteAllAsync();
+
+    }
+}

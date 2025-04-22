cf4dfef061833bdccf80629284e5733318f41091
inc <goats4life@yandex.ru>
2025-04-22 16:33:30 +0300

    feat: add interface for satellite tracking service

---
+++ EPOCH.Application/Interfaces/ISatelliteTrackingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EPOCH.Domain.Entities;
+using EPOCH.Domain.ValueObjects;
+
+namespace EPOCH.Application.Interfaces
+{
+    public interface ISatelliteTrackingService
+    {
+        Task<GeodeticPosition> TrackSatellitePosition(Satellite satellite);
+    }
+}

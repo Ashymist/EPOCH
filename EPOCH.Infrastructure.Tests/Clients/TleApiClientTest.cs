42fbdcbde79ba0b41b32bbf226f2a556f2530e65
inc <goats4life@yandex.ru>
2025-04-24 03:26:46 +0300

    feat: add api client implementation

---
+++ EPOCH.Infrastructure.Tests/Clients/TleApiClientTest.cs
@@ -0,0 +1,28 @@
+﻿using EPOCH.Application.DTOs;
+using EPOCH.Infrastructure.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace EPOCH.Infrastructure.Tests.Clients
+{
+    public class TleApiClientTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public TleApiClientTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public async Task GetAllSatellites_Should_Return_Valid_Satellite_List() {
+            TleApiClient apiClient = new();
+            List<SatelliteDto> dtoList = (List<SatelliteDto>)await apiClient.GetAllSatellites();
+            Assert.True(dtoList.Count > 0);
+        }
+    }
+}
---
+++ EPOCH.Infrastructure/Clients/TleApiClient.cs
@@ -0,0 +1,39 @@
+﻿using EPOCH.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using EPOCH.Application.DTOs;
+
+namespace EPOCH.Infrastructure.Clients
+{
+    public class TleApiClient : ITleApiClient
+    {
+        private readonly HttpClient _httpClient;
+        public TleApiClient() {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<IEnumerable<SatelliteDto>> GetAllSatellites() {
+            var response = _httpClient.GetAsync("https://celestrak.org/NORAD/elements/gp.php?CATNR=25118&FORMAT=tle");
+            string content = await response.Result.Content.ReadAsStringAsync();
+            
+            List<SatelliteDto> dtoList = new();
+
+            string[] contentLines = content.Split('\n').Where(line => !String.IsNullOrWhiteSpace(line)).ToArray();
+
+            for (int i = 0; i < contentLines.Length; i += 3) {
+                dtoList.Add(new SatelliteDto { 
+                    Name = contentLines[i], 
+                    TleLine1 = contentLines[i + 1], 
+                    TleLine2 = contentLines[i + 2] 
+                });
+            }
+
+            return dtoList;
+        }
+
+    }
+}
